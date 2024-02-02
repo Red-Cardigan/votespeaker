25ede9bcedfafabdcc9d9e76b4f5e7fbe3282880
CardiGPT <Jethro.reeve@gmail.com>
2024-02-02 15:53:45 +0000

    styles modifications

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c36ca1c98300756b"; }
+/******/ 		__webpack_require__.h = function() { return "ebdde0c3aac784da"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
